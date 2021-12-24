ddac993a1331d4d6d1ce3606994a41e1263372f1
nickmorris248 <nickmorris248@gmail.com>
2021-12-25 01:41:37 +0700

    finished day 6 part 2

--- day6/day6_part2.py
+++ day6/day6_part2.py
@@ -1,38 +1,45 @@
 
 file_path = "day6_input/"
 
-test_input = '6'  # '8,8,8,8,8'
-
 
 def simulate_fish_ages(file_name, days):
     with open(f'{file_path}{file_name}.txt', encoding='utf8') as f:
         data = f.read()
 
-    fish_list = [int(i) for i in test_input.split(',')]
+    fish_list = [int(i) for i in data.split(',')]
 
     print(fish_list)
 
+    # Keep track of number of fish at each interval
+    interval_counts = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # Loop through fish at the beginning and count the number at each interval
+    for interval in fish_list:
+        interval_counts[interval] += 1
+
+    # Now loop through days and keep track of number of fish at each interval
     for day in range(days):
-        # Cycle through simulated fish and do something based on their internal timer state
-        new_fish_today = []
-        for index, fish_intv in enumerate(fish_list):
-            if fish_intv == 0:
-                # Add new fish with interval timer 8
-                new_fish_today.append(8)
-                # Reset this fish's timer to 6
-                fish_list[index] = 6
-            else:
-                # Reduce interval timer by 1
-                fish_list[index] -= 1
-        # Add the new fish for today onto the end of the list
-        fish_list += new_fish_today
+        # Next day counts
+        next_day = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        # Interval 8
+        next_day[0] = interval_counts[1]
+        next_day[1] = interval_counts[2]
+        next_day[2] = interval_counts[3]
+        next_day[3] = interval_counts[4]
+        next_day[4] = interval_counts[5]
+        next_day[5] = interval_counts[6]
+        next_day[6] = interval_counts[7] + interval_counts[0]
+        next_day[7] = interval_counts[8]
+        next_day[8] = interval_counts[0]
 
-    answer = len(fish_list)
+        # Set the interval counts for next day
+        interval_counts = next_day
 
-    print(answer)
+    answer = sum(interval_counts)
+
+    print(f'Answer: {answer}')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    simulate_fish_ages("example", 60)
+    simulate_fish_ages("fish_ages", 256)
 
